c7714b849ba1bd7fd06f0f4249b076347df3e094
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-05-10 14:43:16 +0530

    post

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -2,3 +2,14 @@ import { comments } from "./data";
 export async function GET() {
     return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+    const { name, age } = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        name,
+        age,
+    };
+    comments.push(newComment);
+    return Response.json(newComment);
+}
